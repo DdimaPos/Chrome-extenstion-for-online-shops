b2f79b6290433d4fe425c8d9b80b8f98009c6350
DdimaPos <postoroncadumitru@gmail.com>
2024-06-16 08:10:21 +0300

    can show the final data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
 const send_button=document.querySelector('.send-button');
+const help_button=document.querySelector('.help-button');
+let current_option=0;
 let site_url;
 let quizz_data;
 
@@ -15,9 +17,22 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 send_button.addEventListener("click", (ev)=>{
     ev.preventDefault()
-    let user_prompt=document.querySelector('.user-prompt').value;
+    let input_field=document.querySelector('.user-prompt')
+    let user_prompt=input_field.value;
+    if(user_prompt==='') return;
+    document.querySelector(".input-form").classList.add("active")
+    setTimeout(() => {
+        document.querySelector(".input-form").classList.remove("active")
+    }, 500);
+    const chatbox=document.querySelector('.chat-window');
+    const messageDiv = document.createElement('div');
+    messageDiv.className = 'message_user';
+    messageDiv.textContent = user_prompt;
+    chatbox.appendChild(messageDiv);
+    chatbox.scrollTop = chatbox.scrollHeight;
     //send prompt to server
     Send_prompt(user_prompt);
+    document.querySelector('.user-prompt').value = '';
     //console.log(user_prompt);
 });
 
@@ -35,7 +50,7 @@ function ProcessUrl (unprocessed){
     if (hostname.startsWith('www.')) {
         hostname = hostname.slice(4);
     }
-
+    console.log(hostname);
     return hostname;
     
 }
@@ -66,7 +81,7 @@ const Send_prompt = (prompt) =>{
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
-        console.log(response)
+        //console.log(response)
         return response.json(); // parses JSON response into native JavaScript objects
     })
     .then(responseData => {
@@ -81,11 +96,14 @@ const Send_prompt = (prompt) =>{
 
 const Send_answers = (answer_arr) => {
     console.log(answer_arr)
+    site_url_processed=ProcessUrl(site_url);
     const url = 'http://192.168.234.4:5000/';
     const data = {
         type: "response",
-        answer_sent: "answer_arr"
+        url: site_url_processed,
+        answer_sent: answer_arr
     };
+    console.log(data);
     fetch(url, {
         method: 'POST', // or 'PUT'
         headers: {
@@ -103,12 +121,30 @@ const Send_answers = (answer_arr) => {
     })
     .then(responseData => {
         console.log(responseData); // handle the data from the response
+        Show_final_data(responseData);
         //update the content of dialog page
         //Update_content(responseData)
     })
     .catch(error => {
         console.error('Error returned', error); // handle error
     });
+}
+
+const Show_final_data = (responseData) =>{
+    const chatbox=document.querySelector('.chat-window');
+    let final_answers=responseData;
+    function displayMessage(message, isBot = true) {
+        const messageDiv = document.createElement('div');
+        messageDiv.className = 'message_bot';
+        messageDiv.innerHTML = message;
+        chatbox.appendChild(messageDiv);
+        chatbox.scrollTop = chatbox.scrollHeight;
+    }
+    let final_message='';
+    final_answers.forEach(el => {
+        final_message += `${el.name} <a href="${el.link}">Link to this product</a><br>`;
+    });
+    displayMessage(final_message)
 }
 
 const Update_content = (responseData) =>{
@@ -126,28 +162,38 @@ const Update_content = (responseData) =>{
     }
 
     function showOptions(step) {
+        help_info=options[steps.length-1]
+        help_button.addEventListener("click", ()=>{
+            console.log(help_info[currentStep])
+        });
+        //add handling of helping information incoming
         if (step < steps.length) {
             const currentOptions = options[steps[step]];
             let optionsText = `Please choose a ${steps[step]}:`;
-            displayMessage(optionsText);
-
-            const optionsContainer = document.createElement('div');
-            optionsContainer.className = 'option-buttons';
-            currentOptions.forEach(option => {
-                const button = document.createElement('button');
-                button.textContent = option;
-                button.addEventListener('click', () => {
-                    users_choices.push(option);
-                    //console.log(users_choices);
-                    displayMessage(option, false);
-                    currentStep++;
-                    optionsContainer.remove();
-                    showOptions(currentStep);
+            setTimeout(() => {
+                displayMessage(optionsText);
+                const optionsContainer = document.createElement('div');
+                optionsContainer.className = 'option-buttons';
+                currentOptions.forEach(option => {
+                    
+                    const button = document.createElement('button');
+                    button.textContent = option;
+                    button.addEventListener('click', () => {
+                        users_choices.push(option);
+                        //console.log(users_choices);
+                        displayMessage(option, false);
+                        current_option++;
+                        currentStep++;
+                        optionsContainer.remove();
+                        showOptions(currentStep);
+                    });
+                    optionsContainer.appendChild(button);
                 });
-                optionsContainer.appendChild(button);
-            });
-            chatbox.appendChild(optionsContainer);
-            chatbox.scrollTop = chatbox.scrollHeight;
+                chatbox.appendChild(optionsContainer);
+                chatbox.scrollTop = chatbox.scrollHeight;
+            }, 500); // 500ms delay
+
+            
         } else {
             Send_answers(users_choices);
             displayMessage("Thank you for making your selections!");
@@ -157,8 +203,6 @@ const Update_content = (responseData) =>{
     // Initialize the chatbot with the first set of options
     showOptions(currentStep);
 }
-
-
 
 
 
